ff1a09dc1915bc34e4008c9b2ea94496153371c7
elvislin <john800116@hotmail.com>
2026-01-12 16:51:09 +0800

    feat: bannerImages constants

---
+++ constants/bannerImages.ts
@@ -0,0 +1,91 @@
+import { assetPath } from '@/lib/utils/asset-path';
+
+export const bannerImages = [
+  {
+    id: 1,
+    src: assetPath('/images/banner/000.jpg'),
+    alt: '活動布置案例',
+  },
+  {
+    id: 2,
+    src: assetPath('/images/banner/001.jpg'),
+    alt: '活動布置案例',
+  },
+  {
+    id: 3,
+    src: assetPath('/images/banner/1070129頂新集團餐飲事業群經營策略會議6.jpg'),
+    alt: '頂新集團餐飲事業群經營策略會議',
+  },
+  {
+    id: 4,
+    src: assetPath('/images/banner/1070130兆豐國際商銀尾牙1.jpg'),
+    alt: '兆豐國際商銀尾牙',
+  },
+  {
+    id: 5,
+    src: assetPath('/images/banner/1070207 Guerlain 嬌蘭尾牙3.jpg'),
+    alt: 'Guerlain 嬌蘭尾牙',
+  },
+  {
+    id: 6,
+    src: assetPath('/images/banner/1070210 PIXNET痞客幫尾牙1.jpg'),
+    alt: 'PIXNET痞客幫尾牙',
+  },
+  {
+    id: 7,
+    src: assetPath(
+      '/images/banner/1070210 刺點皮影親子音樂劇--跟著阿嬤去旅行1.jpg'
+    ),
+    alt: '刺點皮影親子音樂劇',
+  },
+  {
+    id: 8,
+    src: assetPath('/images/banner/1070303大直EMBA11.jpg'),
+    alt: '大直EMBA活動',
+  },
+  {
+    id: 9,
+    src: assetPath('/images/banner/1070408婚禮6.jpg'),
+    alt: '婚禮布置',
+  },
+  {
+    id: 10,
+    src: assetPath('/images/banner/1070616新店優美勝地婚禮1.jpg'),
+    alt: '新店優美勝地婚禮',
+  },
+  {
+    id: 11,
+    src: assetPath('/images/banner/1070627飛牛農協20周年1.jpg'),
+    alt: '飛牛農協20周年慶',
+  },
+  {
+    id: 12,
+    src: assetPath('/images/banner/1070704 求婚佈置 1.jpg'),
+    alt: '浪漫求婚布置',
+  },
+  {
+    id: 13,
+    src: assetPath('/images/banner/1070721 PWC事務所 商會1.jpg'),
+    alt: 'PWC事務所商會活動',
+  },
+  {
+    id: 14,
+    src: assetPath('/images/banner/1070727 生日抓週派對4.1.jpg'),
+    alt: '生日抓週派對',
+  },
+  {
+    id: 15,
+    src: assetPath('/images/banner/1070824 賽車餐廳PTC家庭日 2.jpg'),
+    alt: '賽車餐廳PTC家庭日',
+  },
+  {
+    id: 16,
+    src: assetPath('/images/banner/1111人力銀行尾牙.jpg'),
+    alt: '1111人力銀行尾牙',
+  },
+  {
+    id: 17,
+    src: assetPath('/images/banner/佈置其它_180109_0002.jpg'),
+    alt: '活動布置',
+  },
+];
